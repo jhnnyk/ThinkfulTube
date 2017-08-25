d824d948e3a3011785cd524f3313600a09a58e41
John Knotts <jhnnyk@gmail.com>
2017-08-25 11:59:50 -0600

    initial API response

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 'use strict'
 
 const APIKey = "AIzaSyBL06DgYp59dK1Bt0tUWBoQhusGNXRzKRI"
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
+function getDataFromAPI(searchTerm, callback) {
+  const params = {
+    part: 'snippet',
+    key: `${APIKey}`,
+    q: `${searchTerm}`
+  }
+
+  $.getJSON(YOUTUBE_SEARCH_URL, params, callback)
+}
+
+function renderResult(result) {
+  console.log(result)
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault()
+    const queryTarget = $(event.currentTarget).find('.js-query')
+    const query = queryTarget.val()
+
+    // reset the input
+    queryTarget.val('')
+
+    getDataFromAPI(query, renderResult)
+  })
+}
+
+$(watchSubmit)
